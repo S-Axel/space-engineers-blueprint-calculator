2fbc439f6a293abe400a4087fae29e705dc767e5
S-Axel <sabotieraxel@gmail.com>
2022-11-21 15:56:52 +0100

    refacto: move drag and drop into a new PageFileDnd component

--- src/components/BlueprintCalculator/index.js
+++ src/components/BlueprintCalculator/index.js
@@ -1,19 +1,9 @@
 import PropTypes from 'prop-types';
-import Grid from '@mui/material/Unstable_Grid2';
-import { Card, CardContent, CardHeader } from '@mui/material';
 
-import FileDnd from '../FileDnd';
+import PageFileDnd from '../PageFileDnd';
 
-// eslint-disable-next-line no-unused-vars
 const BlueprintCalculator = ({ blueprint, updateFile }) => (
-  <Grid xs={10} mdOffset={1}>
-    <Card elevation={2}>
-      <CardHeader title="Import a blueprint to be analyzed" titleTypographyProps={{ component: 'h2', variant: 'h5' }} />
-      <CardContent>
-        <FileDnd updateFile={updateFile} />
-      </CardContent>
-    </Card>
-  </Grid>
+  blueprint || <PageFileDnd updateFile={updateFile} />
 );
 
 BlueprintCalculator.propTypes = {
---
+++ src/components/PageFileDnd/index.js
@@ -0,0 +1,22 @@
+import { Card, CardContent, CardHeader } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import PropTypes from 'prop-types';
+
+import FileDnd from '../FileDnd';
+
+const PageFileDnd = ({ updateFile }) => (
+  <Grid xs={10} mdOffset={1}>
+    <Card elevation={2}>
+      <CardHeader title="Import a blueprint to be analyzed" titleTypographyProps={{ component: 'h2', variant: 'h5' }} />
+      <CardContent>
+        <FileDnd updateFile={updateFile} />
+      </CardContent>
+    </Card>
+  </Grid>
+);
+
+PageFileDnd.propTypes = {
+  updateFile: PropTypes.func.isRequired,
+};
+
+export default PageFileDnd;
