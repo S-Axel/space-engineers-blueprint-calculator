e1dd5c4784458d3b35f7782f2062e65a797d85e3
S-Axel <sabotieraxel@gmail.com>
2022-12-08 08:53:24 +0100

    fix: console error with CustomCardHeader requiring and using children prop while it is not needed

--- src/components/CustomCardHeader/index.js
+++ src/components/CustomCardHeader/index.js
@@ -1,18 +1,15 @@
 import { CardHeader } from '@mui/material';
 import PropTypes from 'prop-types';
 
-const CustomCardHeader = ({ children, sx, titleTypographyProps, ...props }) => (
+const CustomCardHeader = ({ sx, titleTypographyProps, ...props }) => (
   <CardHeader
     sx={{ ...sx }}
     titleTypographyProps={{ ...titleTypographyProps, variant: 'h6' }}
     {...props}
-  >
-    {children}
-  </CardHeader>
+  />
 );
 
 CustomCardHeader.propTypes = {
-  children: PropTypes.node.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
   sx: PropTypes.object,
   // eslint-disable-next-line react/forbid-prop-types
