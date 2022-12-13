93809189b308a1d3352172a6b7da6e90c34a20c2
S-Axel <sabotieraxel@gmail.com>
2022-12-13 20:03:07 +0100

    refactor: set reportWebVitals.ts and index.tsx files to CRA typescript version

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,9 @@ import '@fontsource/roboto/700.css';
 import reportWebVitals from './reportWebVitals';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
   <React.StrictMode>
     <App />
